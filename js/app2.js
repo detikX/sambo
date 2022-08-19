252e4b09c653ccde29bda60208af911a85bfa4f6
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-08-19 13:56:20 +0700

    update

--- js/app2.js
+++ js/app2.js
@@ -74,103 +74,149 @@ $.ajax({
         // response.events[i].id.push(response.events[i].id.splice(i, 1)[0]);
         // console.log(i + ': ' + response.events[i].id.join(' '));
         // nul.push(response.events)
-        var dates = response.events[i].id;
-        // console.log(dates);
-        $(".dedidedi").append(`
-          <div class="font-bold">${dates}</div>
+        var id_ = response.events[i].id;
+        var headline = response.events[i].text.headline;
+        var desc = response.events[i].text.text;
+        var url_media = response.events[i].text.url_media;
+        // console.log(id_);
+        $(".ag-timeline_list").append(`
+        <div class="js-timeline_item ag-timeline_item">
+            <div class="ag-timeline-card_box">
+              <div class="js-timeline-card_point-box
+                ag-timeline-card_point-box">
+                <div class="ag-timeline-card_point">${id_}</div>
+              </div>
+              <div class="ag-timeline-card_meta-box">
+                <div class="ag-timeline-card_meta">${headline}</div>
+              </div>
+            </div>
+            <div class="ag-timeline-card_item">
+              <div class="ag-timeline-card_inner">
+                <div class="ag-timeline-card_img-box">
+                  <img
+                    src="${url_media}"
+                    class="ag-timeline-card_img" />
+                </div>
+                <div class="ag-timeline-card_info">
+                  <div class="ag-timeline-card_title">${headline}</div>
+                  <div class="ag-timeline-card_desc">
+                    ${desc}
+                  </div>
+                </div>
+              </div>
+              <div class="ag-timeline-card_arrow"></div>
+            </div>
+          </div>
+        `)
+      }
+      if (response.events[i].id % 2 === 0) {
+        // response.events[i].id.push(response.events[i].id.splice(i, 1)[0]);
+        // console.log(i + ': ' + response.events[i].id.join(' '));
+        // nul.push(response.events)
+        var id_ = response.events[i].id;
+        var headline = response.events[i].text.headline;
+        var desc = response.events[i].text.text;
+        var url_media = response.events[i].text.url_media;
+        // console.log(id_);
+        $(".ag-timeline_list").append(`
+        <div class="js-timeline_item ag-timeline_item">
+        <div class="ag-timeline-card_box">
+          <div class="ag-timeline-card_meta-box">
+            <div class="ag-timeline-card_meta">${headline}</div>
+          </div>
+          <div class="js-timeline-card_point-box
+            ag-timeline-card_point-box">
+            <div class="ag-timeline-card_point">${id_}</div>
+          </div>
+        </div>
+        <div class="ag-timeline-card_item">
+          <div class="ag-timeline-card_inner">
+            <div class="ag-timeline-card_img-box">
+              <img
+                src="${url_media}"
+                class="ag-timeline-card_img" alt="" />
+            </div>
+            <div class="ag-timeline-card_info">
+              <div class="ag-timeline-card_title">${headline}</div>
+              <div class="ag-timeline-card_desc">
+                ${desc}
+              </div>
+            </div>
+          </div>
+          <div class="ag-timeline-card_arrow"></div>
+        </div>
+      </div>
         `)
       }
     }
-    // var odds = response.events[i].id.filter(number => {
-    //   return number % 2 !== 0;
-    // });
-    // // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-
-    // console.log(odds); 
-
-  
-  // console.log(response.events);
-    // for (a = 0; a < response.events.length; a++) {
-
-
-    //   $(".dedidedi").append(`
-
-    //     <div class="dedidedi__1">
-    //       <div class="odd"></div>
-    //       <div class="even"></div>
-    //     </div>
-
-    //   `)
-    //   // console.log(response.events[a].start_date);
-
-    // }
+    $(window).on('scroll', function () {
+      fnOnScroll();
+    });
+    
+    $(window).on('resize', function () {
+      fnOnResize();
+    });
+    
+    
+    var agTimeline = $('.js-timeline'),
+      agTimelineLine = $('.js-timeline_line'),
+      agTimelineLineProgress = $('.js-timeline_line-progress'),
+      agTimelinePoint = $('.js-timeline-card_point-box'),
+      agTimelineItem = $('.js-timeline_item'),
+      agOuterHeight = $(window).outerHeight(),
+      agHeight = $(window).height(),
+      f = -1,
+      agFlag = false;
+    
+    function fnOnScroll() {
+      agPosY = $(window).scrollTop();
+    
+      fnUpdateFrame();
+    }
+    
+    function fnOnResize() {
+      agPosY = $(window).scrollTop();
+      agHeight = $(window).height();
+    
+      fnUpdateFrame();
+    }
+    
+    function fnUpdateWindow() {
+      agFlag = false;
+    
+      agTimelineLine.css({
+        top: agTimelineItem.first().find(agTimelinePoint).offset().top - agTimelineItem.first().offset().top,
+        bottom: agTimeline.offset().top + agTimeline.outerHeight() - agTimelineItem.last().find(agTimelinePoint).offset().top
+      });
+    
+      f !== agPosY && (f = agPosY, agHeight, fnUpdateProgress());
+    }
+    
+    function fnUpdateProgress() {
+      var agTop = agTimelineItem.last().find(agTimelinePoint).offset().top;
+    
+      i = agTop + agPosY - $(window).scrollTop();
+      a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
+      n = agPosY - a + agOuterHeight / 2;
+      i <= agPosY + agOuterHeight / 2 && (n = i - a);
+      agTimelineLineProgress.css({ height: n + "px" });
+    
+      agTimelineItem.each(function () {
+        var agTop = $(this).find(agTimelinePoint).offset().top;
+    
+        (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
+      })
+    }
+    
+    function fnUpdateFrame() {
+      agFlag || requestAnimationFrame(fnUpdateWindow);
+      agFlag = true;
+    }
+    
 
   }
 })
 
-$(window).on('scroll', function () {
-  fnOnScroll();
-});
-
-$(window).on('resize', function () {
-  fnOnResize();
-});
-
-
-var agTimeline = $('.js-timeline'),
-  agTimelineLine = $('.js-timeline_line'),
-  agTimelineLineProgress = $('.js-timeline_line-progress'),
-  agTimelinePoint = $('.js-timeline-card_point-box'),
-  agTimelineItem = $('.js-timeline_item'),
-  agOuterHeight = $(window).outerHeight(),
-  agHeight = $(window).height(),
-  f = -1,
-  agFlag = false;
-
-function fnOnScroll() {
-  agPosY = $(window).scrollTop();
-
-  fnUpdateFrame();
-}
-
-function fnOnResize() {
-  agPosY = $(window).scrollTop();
-  agHeight = $(window).height();
-
-  fnUpdateFrame();
-}
-
-function fnUpdateWindow() {
-  agFlag = false;
-
-  agTimelineLine.css({
-    top: agTimelineItem.first().find(agTimelinePoint).offset().top - agTimelineItem.first().offset().top,
-    bottom: agTimeline.offset().top + agTimeline.outerHeight() - agTimelineItem.last().find(agTimelinePoint).offset().top
-  });
-
-  f !== agPosY && (f = agPosY, agHeight, fnUpdateProgress());
-}
-
-function fnUpdateProgress() {
-  var agTop = agTimelineItem.last().find(agTimelinePoint).offset().top;
-
-  i = agTop + agPosY - $(window).scrollTop();
-  a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
-  n = agPosY - a + agOuterHeight / 2;
-  i <= agPosY + agOuterHeight / 2 && (n = i - a);
-  agTimelineLineProgress.css({ height: n + "px" });
-
-  agTimelineItem.each(function () {
-    var agTop = $(this).find(agTimelinePoint).offset().top;
-
-    (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
-  })
-}
-
-function fnUpdateFrame() {
-  agFlag || requestAnimationFrame(fnUpdateWindow);
-  agFlag = true;
-}
 
 
