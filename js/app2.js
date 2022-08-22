5b9c2734fe086f9f286176f23e28032b04b4695a
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-08-22 11:50:47 +0700

    updated

--- js/app2.js
+++ js/app2.js
@@ -220,7 +220,60 @@ $.ajax({
     
 
   }
-})
+});
+
+$("#scroll-top").hide();
+// fade in #back-top
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#scroll-top').fadeIn();
+    } else {
+      $('#scroll-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#scroll-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+  });
+});
 
 
+$(".pistol").click(()=>{
+  $('html, body').animate({
+    scrollTop: $(".ag-timeline-block").offset().top
+}, 2000);
+});
 
+
+const pauseVar = "fa-pause-circle";
+const playVar = "fa-play-circle";
+
+const btns = document.querySelectorAll(".all");
+
+function onChange(event) {
+  const buttonElement = event.currentTarget;
+
+  const isPlayButton = buttonElement.classList.contains(playVar);
+
+  if (isPlayButton) {
+    buttonElement.classList.remove(playVar);
+    buttonElement.classList.add(pauseVar);
+    $("#play-button").removeClass("animate__infinite");
+    $("#myAudio").get(0).play();
+  } else {
+    buttonElement.classList.remove(pauseVar);
+    buttonElement.classList.add(playVar);
+    $("#play-button").addClass("animate__infinite");
+    $("#myAudio").get(0).pause();
+  }
+
+  // You can also use .toggle function on classList as mentioned by the person in other answer
+}
+
+// query selector all returns a list of nodes, therefore we need to iterate over it and attach an event listener to each button seperatly
+btns.forEach((btn) => {
+  btn.addEventListener("click", onChange);
+});
